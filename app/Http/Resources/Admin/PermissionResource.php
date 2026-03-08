b3206f97516a3a5c09e56b8b3fec2b8c4a1dc3cf
mustfamoolan <almoq3m@gmail.com>
2026-03-08 06:49:56 +0300

    Fix duplicate migrations causing server crash

--- app/Http/Resources/Admin/PermissionResource.php
+++ app/Http/Resources/Admin/PermissionResource.php
@@ -17,6 +17,14 @@ class PermissionResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
+            'roles' => $this->whenLoaded('roles', function () {
+                return $this->roles->map(function ($role) {
+                    return [
+                        'id' => $role->id,
+                        'name' => $role->name,
+                    ];
+                });
+            }),
             'created_at' => $this->created_at?->toDateTimeString(),
             'updated_at' => $this->updated_at?->toDateTimeString(),
         ];
