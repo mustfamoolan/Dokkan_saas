83d22c9027e7cf149d83f0f4fb47e2345dfded77
mustfamoolan <almoq3m@gmail.com>
2026-03-14 21:53:10 +0300

    Update project name to Dokkan and update AdminUserSeeder

--- database/seeders/Admin/AdminUserSeeder.php
+++ database/seeders/Admin/AdminUserSeeder.php
@@ -16,13 +16,13 @@ class AdminUserSeeder extends Seeder
     {
         // Create default admin user
         $admin = User::firstOrCreate(
-            ['phone' => '07709078728'],
-            [
-                'name' => 'مدير النظام',
-                'password' => Hash::make('07709078728'),
-                'type' => UserType::ADMIN,
-                'is_active' => true,
-            ]
+        ['phone' => '07742209251'],
+        [
+            'name' => 'مدير النظام',
+            'password' => Hash::make('12345678'),
+            'type' => UserType::ADMIN,
+            'is_active' => true,
+        ]
         );
 
         // Assign admin role if it exists
@@ -31,7 +31,7 @@ class AdminUserSeeder extends Seeder
         }
 
         $this->command->info('تم إنشاء مستخدم المدير بنجاح');
-        $this->command->info('رقم الهاتف: 07709078728');
-        $this->command->info('كلمة المرور: 07709078728');
+        $this->command->info('رقم الهاتف: 07742209251');
+        $this->command->info('كلمة المرور: 12345678');
     }
 }
