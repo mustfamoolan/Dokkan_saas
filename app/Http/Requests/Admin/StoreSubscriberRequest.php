8478a4be76d91cedaf044d10d8cdc7ad01ac03b9
mustfamoolan <almoq3m@gmail.com>
2026-03-17 14:20:29 +0300

    Fix duplicate migrations causing server crash

--- app/Http/Requests/Admin/StoreSubscriberRequest.php
+++ app/Http/Requests/Admin/StoreSubscriberRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests\Admin;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Auth;
 
 class StoreSubscriberRequest extends FormRequest
 {
--- app/Http/Requests/Admin/UpdatePlanFeaturesRequest.php
+++ app/Http/Requests/Admin/UpdatePlanFeaturesRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests\Admin;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
 
 class UpdatePlanFeaturesRequest extends FormRequest
 {
